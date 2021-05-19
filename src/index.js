7fb36bf7b5cc146e0774404204f1c92555e71cf7
Lluis Bagur <lluisbagur09@gmail.com>
2021-05-19 14:59:39 +0200

    starting project, and configurate librarys, router, redux and i18n

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import i18next from "i18next"
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import {I18nextProvider} from "react-i18next"
 import * as serviceWorker from './serviceWorker';
+import history from './app/history';
+
+import common_es from "./i18n/es.json"
+import common_en from "./i18n/en.json"
+
+
+i18next.init({
+    interpolation: {escapeValue: false},
+    lng: 'es',
+    nsSeparator: false,
+    keySeparator: false,
+    resources: {
+        es: {
+            common: common_es
+        },
+        en: {
+            common: common_en
+        },
+    },
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <I18nextProvider i18n={i18next}>
     <Provider store={store}>
+      <Router history={history}>
       <App />
+      </Router>
     </Provider>
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
